f234582383512d9ba65c5a44a89920161e88ca49
Onur Küçükkeçe <onurkucukkece@gmail.com>
2016-08-27 15:35:01 +0300

    parasute api credential to spec heler

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,3 +3,12 @@ Coveralls.wear!
 
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'parasut'
+require 'parasut_api'
+
+OAUTH_CLIENT = OAuth2::Client.new(
+  ENV['PARASUT_API_CLIENT_ID'],
+  ENV['PARASUT_API_CLIENT_SECRET'],
+  site: ENV['PARASUT_API_AUTH_ENDPOINT']
+)
+
+OAUTH_TOKEN = OAuth2::AccessToken.from_hash OAUTH_CLIENT, access_token: ENV['PARASUT_API_ACCESS_TOKEN']
