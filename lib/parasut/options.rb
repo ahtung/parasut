3d6c91b6bd9834905b5801e543002548b7dd6fd1
Onur Küçükkeçe <onurkucukkece@gmail.com>
2016-08-27 13:02:50 +0300

    added options.rb

---
+++ lib/parasut/options.rb
@@ -0,0 +1,12 @@
+# lib/parasut/options.rb
+module Parasut
+  class Options
+    attr_accessor :client_id
+    attr_accessor :client_secret
+    attr_accessor :username
+    attr_accessor :password
+    attr_accessor :company_id
+    attr_accessor :grant_type
+    attr_accessor :redirect_uri
+  end
+end
