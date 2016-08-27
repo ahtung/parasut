ac48a422f2dad6a75f4bacf84dfd30c7ee70db18
Onur Küçükkeçe <onurkucukkece@gmail.com>
2016-08-27 15:54:12 +0300

    setting client argumanet

--- lib/parasut/client.rb
+++ lib/parasut/client.rb
@@ -15,12 +15,12 @@ module Parasut
       response = connection.post do |req|
         req.headers['Content-Type'] = 'application/x-www-form-urlencoded'
         req.body = {
-          client_id: URI::encode options.client_id,
-          client_secret: URI::encode options.client_secret,
-          username: URI::encode options.username,
-          password: URI::encode options.password,
-          grant_type: URI::encode options.grant_type,
-          redirect_uri: URI::encode options.redirect_uri
+          client_id: URI::encode(options.client_id),
+          client_secret: URI::encode(options.client_secret),
+          username: URI::encode(options.username),
+          password: URI::encode(options.password),
+          grant_type: URI::encode(options.grant_type),
+          redirect_uri: URI::encode(options.redirect_uri)
         }
         Rails.logger.info "Body #{req.body.inspect}"
       end
