caa1259a96c60459962744b80bab901bfb19a628
Onur Küçükkeçe <onurkucukkece@gmail.com>
2016-08-27 14:14:05 +0300

    added initialiser method to options class

--- lib/parasut/options.rb
+++ lib/parasut/options.rb
@@ -9,5 +9,16 @@ module Parasut
     attr_accessor :company_id
     attr_accessor :grant_type
     attr_accessor :redirect_uri
+
+    # Initializer
+    def initialize
+      @client_id = 'client_id'
+      @client_secret = 'client_secret'
+      @username = 'username'
+      @password = 'password'
+      @company_id = 'company_id'
+      @grant_type = 'grant_type'
+      @redirect_uri = 'redirect_uri'
+    end
   end
 end
