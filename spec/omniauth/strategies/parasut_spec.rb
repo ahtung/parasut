7cc2bef46e24b2c2fbc33528b41f4b73f5b02a73
Onur Küçükkeçe <onurkucukkece@gmail.com>
2016-08-27 15:58:07 +0300

    comment out everything related to oauth

--- spec/omniauth/strategies/parasut_spec.rb
+++ spec/omniauth/strategies/parasut_spec.rb
@@ -1,10 +1,10 @@
-require 'spec_helper'
-
-describe Omniauth::Strategies::Parasut do
-  xit 'should be able to authenticate API' do
-    api = ParasutApi.new(OAUTH_TOKEN, 'http://api.parasut.dev:3000/v1')
-    user = api.current_user
-
-    expect(user.is_a? ParasutApi::User).to eq(true)
-  end
-end
+# require 'spec_helper'
+#
+# describe Omniauth::Strategies::Parasut do
+#   xit 'should be able to authenticate API' do
+#     api = ParasutApi.new(OAUTH_TOKEN, 'http://api.parasut.dev:3000/v1')
+#     user = api.current_user
+#
+#     expect(user.is_a? ParasutApi::User).to eq(true)
+#   end
+# end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,12 +3,12 @@ Coveralls.wear!
 
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'parasut'
-require 'oauth2'
+# require 'oauth2'
 
-OAUTH_CLIENT = OAuth2::Client.new(
-  ENV['PARASUT_API_CLIENT_ID'],
-  ENV['PARASUT_API_CLIENT_SECRET'],
-  site: ENV['PARASUT_API_AUTH_ENDPOINT']
-)
-
-OAUTH_TOKEN = OAuth2::AccessToken.from_hash OAUTH_CLIENT, access_token: ENV['PARASUT_API_ACCESS_TOKEN']
+# OAUTH_CLIENT = OAuth2::Client.new(
+#   ENV['PARASUT_API_CLIENT_ID'],
+#   ENV['PARASUT_API_CLIENT_SECRET'],
+#   site: ENV['PARASUT_API_AUTH_ENDPOINT']
+# )
+#
+# OAUTH_TOKEN = OAuth2::AccessToken.from_hash OAUTH_CLIENT, access_token: ENV['PARASUT_API_ACCESS_TOKEN']
