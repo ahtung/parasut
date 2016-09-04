1fba8a4f7ef468d20c46dd3528aa73f09b3a3e85
Dunya Kirkali <dunyakirkali@gmail.com>
2016-09-04 17:25:15 +0200

    Increase coverage

--- lib/parasut/client.rb
+++ lib/parasut/client.rb
@@ -5,15 +5,16 @@ module Parasut
     API_VERSION = 'v1'
 
     def initialize(access_token)
-      strategy = OmniAuth::Strategies::Parasut.new(nil, ENV['PARASUT_CLIENT_ID'], ENV['PARASUT_CLIENT_SECRET'])
+      strategy = OmniAuth::Strategies::Parasut.new(ENV['PARASUT_CLIENT_ID'], ENV['PARASUT_CLIENT_SECRET'], authorize_params: {:foo => "bar", :baz => "zip"},
+        token_params: { username: 'dunyakirkali', password: 'dE3kI812', grant_type: 'password' })
       @access_token = strategy.client.auth_code.get_token('password', redirect_uri: 'urn:ietf:wg:oauth:2.0:oob')
       RestClient.add_before_execution_proc do |req, params|
         @access_token.sign! req
       end
     end
 
-    def health
-      RestClient.get('/')
+    def me
+      RestClient.get("/#{API_VERSION}/me")
     end
   end
 end
