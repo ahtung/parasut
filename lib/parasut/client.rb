6b6f4ade983c22523788a5a2280642dbf0932de2
Dunya Kirkali <dunyakirkali@gmail.com>
2016-09-04 19:06:24 +0200

    Correct spec

--- lib/parasut/client.rb
+++ lib/parasut/client.rb
@@ -2,28 +2,24 @@ module Parasut
   # Parasut::Client
   class Client
     API_VERSION = 'v1'.freeze
-    BASE_URL = 'https://api.parasut.com/oauth/token'.freeze
+    BASE_URL = 'https://api.parasut.com'.freeze
 
     def initialize
     end
 
     def me
-      RestClient.get("#{oauth_strategy.default_options[:client_options][:site]}/#{API_VERSION}/me", Authorization: "Bearer #{refresh_token['access_token']}")
-    end
-
-    def oauth_strategy
-      OmniAuth::Strategies::Parasut
+      RestClient.get("#{BASE_URL}/#{API_VERSION}/me", Authorization: "Bearer #{refresh_token['access_token']}")
     end
 
     private
 
     def refresh_token
-      resp = RestClient.post("#{BASE_URL}?client_id=#{ENV['PARASUT_CLIENT_ID']}&client_secret=#{ENV['PARASUT_CLIENT_SECRET']}&grant_type=refresh_token&refresh_token=#{password['refresh_token']}", {})
+      resp = RestClient.post("#{BASE_URL}/oauth/token?client_id=#{ENV['PARASUT_CLIENT_ID']}&client_secret=#{ENV['PARASUT_CLIENT_SECRET']}&grant_type=refresh_token&refresh_token=#{password['refresh_token']}", {})
       JSON.parse(resp)
     end
 
     def password
-      resp = RestClient.post("#{BASE_URL}?client_id=#{ENV['PARASUT_CLIENT_ID']}&client_secret=#{ENV['PARASUT_CLIENT_SECRET']}&username=dunyakirkali@ahtung.co&password=dE3kI812&grant_type=password&redirect_uri=urn:ietf:wg:oauth:2.0:oob", {})
+      resp = RestClient.post("#{BASE_URL}/oauth/token?client_id=#{ENV['PARASUT_CLIENT_ID']}&client_secret=#{ENV['PARASUT_CLIENT_SECRET']}&username=#{ENV['PARASUT_USERNAME']}&password=dE3kI812&grant_type=password&redirect_uri=urn:ietf:wg:oauth:2.0:oob", {})
       JSON.parse(resp)
     end
   end
