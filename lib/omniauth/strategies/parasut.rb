22f35f3e74244a0efc401a1d1124aa6997895d68
Dunya Kirkali <dunyakirkali@gmail.com>
2016-09-04 16:30:29 +0200

    Refactor client code to oauth2 strategy

--- lib/omniauth/strategies/parasut.rb
+++ lib/omniauth/strategies/parasut.rb
@@ -2,7 +2,11 @@ module OmniAuth
   module Strategies
     class Parasut < OmniAuth::Strategies::OAuth2
       option :name, 'parasut'
-      option :client_options, { site: 'https://api.parasut.com' }
+      option :client_options, {
+        site: 'https://api.parasut.com',
+        authorize_url: 'https://api.parasut.com/oauth/authorize',
+        token_url: 'https://api.parasut.com/oauth/token'
+      }
 
       uid { api.current_user.id }
       info { api.current_user }
--- lib/parasut/client.rb
+++ lib/parasut/client.rb
@@ -2,27 +2,18 @@ module Parasut
   class Client
     attr_reader :access_token
 
-    BASE_URL = 'https://api.parasut.com'
-    AUTH_PATH = '/oauth/authorize'
-    TOKEN_PATH = '/oauth/token'
-    API_VERSION = '/v1'
+    API_VERSION = 'v1'
 
     def initialize(access_token)
       strategy = OmniAuth::Strategies::Parasut.new(nil, ENV['PARASUT_CLIENT_ID'], ENV['PARASUT_CLIENT_SECRET'])
-      # @access_token = access_token
-      # RestClient.add_before_execution_proc do |req, params|
-      #   @access_token.sign! req
-      # end
+      @access_token = strategy.client.auth_code.get_token('password', redirect_uri: 'urn:ietf:wg:oauth:2.0:oob')
+      RestClient.add_before_execution_proc do |req, params|
+        @access_token.sign! req
+      end
     end
 
     def health
-      RestClient.get(api_root_url)
-    end
-
-    private
-
-    def api_root_url
-      [BASE_URL, API_VERSION].join
+      RestClient.get('/')
     end
   end
 end
