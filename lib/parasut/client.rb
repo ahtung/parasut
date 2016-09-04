de39db38c573661d48d8d964e995fa9965f04a56
onur <onurkucukkece@gmail.com>
2016-09-04 20:35:15 +0300

    improved params

--- lib/parasut/client.rb
+++ lib/parasut/client.rb
@@ -14,13 +14,33 @@ module Parasut
     private
 
     def refresh_token
-      resp = RestClient.post("#{BASE_URL}/oauth/token?client_id=#{Parasut.options.client_id}&client_secret=#{Parasut.options.client_secret}&grant_type=refresh_token&refresh_token=#{password['refresh_token']}", {})
+      resp = RestClient.post("#{BASE_URL}?#{URI.encode_www_form(refresh_token_params)}", {})
       JSON.parse(resp)
     end
 
     def password
-      resp = RestClient.post("#{BASE_URL}/oauth/token?client_id=#{Parasut.options.client_id}&client_secret=#{Parasut.options.client_secret}&username=#{Parasut.options.username}&password=#{Parasut.options.password}&grant_type=password&redirect_uri=urn:ietf:wg:oauth:2.0:oob", {})
+      resp = RestClient.post("#{BASE_URL}?#{URI.encode_www_form(password_params)}", {})
       JSON.parse(resp)
+    end
+
+    def refresh_token_params
+      {
+        client_id: Parasut.options.client_id,
+        client_secret: Parasut.options.client_secret,
+        grant_type: 'refresh_token',
+        refresh_token: password['refresh_token']
+      }
+    end
+
+    def password_params
+      {
+        client_id: Parasut.options.client_id,
+        client_secret: Parasut.options.client_secret,
+        username: Parasut.options.username,
+        password: Parasut.options.password,
+        grant_type: 'password',
+        redirect_uri: 'urn:ietf:wg:oauth:2.0:oob'
+      }
     end
   end
 end
