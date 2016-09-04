3abbeb8251fdec0f56f7d492e68e22b13e901128
onur <onurkucukkece@gmail.com>
2016-09-04 18:56:16 +0300

    set attributes in contact initializer

--- lib/parasut/contact.rb
+++ lib/parasut/contact.rb
@@ -11,7 +11,18 @@ module Parasut
     attr_accessor *ATTRIBUTES
 
     # Initializer
-    def initialize
+    def initialize(options)
+      @id = options[:id]
+      @name = options[:name]
+      @contact_type = options[:contact_type]
+      @email = options[:email]
+      @balance = options[:balance]
+      @tax_office = options[:tax_office]
+      @tax_number = options[:tax_number]
+      @archived = options[:archived]
+      @category = options[:category]
+      @address = options[:address]
+      @contact_people = options[:contact_people]
     end
   end
 end
