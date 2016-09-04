c1beaf9eb8f8d2ca96ea22c4544ee72e6277401e
onur <onurkucukkece@gmail.com>
2016-09-04 19:13:18 +0300

    boyscout

--- spec/parasut/contact_spec.rb
+++ spec/parasut/contact_spec.rb
@@ -1,19 +1,11 @@
 require 'spec_helper'
 
 describe Parasut::Contact do
-  let(:options) do
-  end
-
-  #:id, :name, :contact_type, :email, :balance, :tax_office,
-  #:tax_number, :archived, :category, :address, :contact_people
-
   # Attributes
   Parasut::Contact::ATTRIBUTES.each do |attribute|
-    describe "should have attribute attr_accessor #{attribute.to_s}" do
-      it attribute.to_s do
-        contact = Parasut::Contact.new(attribute => 'Some string')
-        expect(contact.send(attribute.to_s)).to eq('Some string')
-      end
+    it "should have attribute attr_accessor #{attribute.to_s}" do
+      contact = Parasut::Contact.new(attribute => 'Some string')
+      expect(contact.send(attribute.to_s)).to eq('Some string')
     end
   end
 
