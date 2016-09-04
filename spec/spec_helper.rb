233086b6ce76d38b70a536bb5fe30dd222f63bb4
onur <onurkucukkece@gmail.com>
2016-09-04 20:21:03 +0300

    added before suite for parasite option class in spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,3 +12,15 @@ require 'parasut'
 # )
 #
 # OAUTH_TOKEN = OAuth2::AccessToken.from_hash OAUTH_CLIENT, access_token: ENV['PARASUT_API_ACCESS_TOKEN']
+
+RSpec.configure do |config|
+  config.before(:suite) do
+    Parasut.options do |options|
+      options.client_id = ENV['PARASUT_CLIENT_ID']
+      options.client_secret = ENV['PARASUT_CLIENT_SECRET']
+      options.username = ENV['PARASUT_USERNAME']
+      options.password = ENV['PARASUT_PASSWORD']
+      options.company_id = 'company_id'
+    end
+  end
+end
