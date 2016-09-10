3bf87d10c6994e6a12d9c2ce23488242b3f55102
onur <onurkucukkece@gmail.com>
2016-09-11 00:10:16 +0300

    cleaned produt

--- lib/parasut/product.rb
+++ lib/parasut/product.rb
@@ -1,7 +1,6 @@
 # lib/parasut/product.rb
 module Parasut
   # Product
-    PATH = 'products'.freeze
   class Product < Parasut::Base
     ATTRIBUTES = [:code, :name, :vat_rate, :currency, :list_price, :archived, :category].freeze
 
@@ -12,27 +11,8 @@ module Parasut
       self.id = options['id']
     end
 
-    def self.all
-      JSON.parse(Parasut::Client.get(collection_path))['items']
-    end
-
-    def self.find(id)
-      product = JSON.parse(Parasut::Client.get(instance_path(id)))['product']
-      new(contact)
-    end
-
-    def delete
-      JSON.parse(Parasut::Client.delete(self.class.instance_path(id)))['success'] == 'OK'
-    end
-
-    private
-
-    def self.instance_path(id)
-      "#{collection_path}/#{id}"
-    end
-
-    def self.collection_path
-      "100174/#{PATH}"
+    def self.path
+      'products'
     end
   end
 end
