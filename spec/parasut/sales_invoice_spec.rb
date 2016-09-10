d4610dc8297f4f56bd546e7b36f43c739764fc0e
Dunya Kirkali <dunyakirkali@gmail.com>
2016-09-10 23:11:31 +0200

    Add Sales invoice specs

--- spec/parasut/sales_invoice_spec.rb
+++ spec/parasut/sales_invoice_spec.rb
@@ -1,11 +1,41 @@
 require 'spec_helper'
 
-describe Parasut::SalesInvoice do
-  # SalesInvoice
+RSpec.describe Parasut::SalesInvoice do
+  subject { described_class.new }
+
+  # Attributes
   Parasut::SalesInvoice::ATTRIBUTES.each do |attribute|
-    it "should have attribute attr_accessor #{attribute.to_s}" do
-      sales_invoice = Parasut::SalesInvoice.new(attribute => 'Some string')
-      expect(sales_invoice.send(attribute.to_s)).to eq('Some string')
+    xit { should have_attr_accessor(attribute.to_sym) }
+  end
+
+  # Class methods
+  describe '.' do
+    describe 'all' do
+      it 'returns all contacts' do
+        expect(Parasut::SalesInvoice.all.length).not_to eq(0)
+      end
+    end
+
+    describe 'create' do
+      it 'creates a new contact' do
+        expect(Parasut::SalesInvoice.create(name: 'Dunya')).to eq(true)
+      end
+    end
+
+    describe 'find' do
+      it 'returns a contact instance' do
+        expect(Parasut::SalesInvoice.find(1_205_839)).to be_instance_of(described_class)
+      end
+    end
+  end
+
+  # Instance methods
+  describe '#' do
+    describe 'delete' do
+      it 'deletes a contact' do
+        contact = Parasut::SalesInvoice.find(1_205_839)
+        expect(contact.delete).to eq(true)
+      end
     end
   end
 end
