6371ea3ef5ac7e92fe90bdd657042baed694aacc
onur <onurkucukkece@gmail.com>
2016-09-17 18:24:57 +0300

    fix contacts spec

--- spec/parasut/contact_spec.rb
+++ spec/parasut/contact_spec.rb
@@ -7,11 +7,11 @@ RSpec.describe Parasut::Contact do
     let(:contact) { described_class.find(1) }
 
     it 'outstanding_payments' do
-      expect(contact.outstanding_payments).to be_instance_of(Parasut::Payment)
+      expect(contact.outstanding_payments.first).to be_instance_of(Parasut::Payment)
     end
 
     it 'past_transactions' do
-      expect(contact.past_transactions).to be_instance_of(Parasut::Transaction)
+      expect(contact.past_transactions.first).to be_instance_of(Parasut::Transaction)
     end
   end
 end
