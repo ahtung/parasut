0842727bd007fa3b2d99b4a5e808815f5a9f09d8
Dunya Kirkali <dunyakirkali@gmail.com>
2016-09-17 18:06:11 +0200

    Add todo

--- lib/parasut.rb
+++ lib/parasut.rb
@@ -59,6 +59,7 @@ class ParasutParser < Her::Middleware::DefaultParseJSON
   def parse(body)
     json = parse_json(body)
     data = json
+    # TODO: (dunyakirkali) :transactions exception should be removed
     data = data[:transactions].present? ? data[:transactions] : data if data[:account].blank?
     data = data[:items].present? ? data[:items] : data
     {
