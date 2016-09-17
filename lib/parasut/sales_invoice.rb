58159d6a197e6da229aff643b88b9f57ee7b32de
Dunya Kirkali <dunyakirkali@gmail.com>
2016-09-17 16:47:10 +0200

    Remove hardcoded id

--- lib/parasut/sales_invoice.rb
+++ lib/parasut/sales_invoice.rb
@@ -7,13 +7,13 @@ module Parasut
     parse_root_in_json true
 
     def e_document_status
-      self.class.get_raw("sales_invoices/1#{id}/e_document_status") do |parsed_data, response|
+      self.class.get_raw("sales_invoices/#{id}/e_document_status") do |parsed_data, response|
         parsed_data[:data]
       end
     end
 
     def e_document_type
-      self.class.get_raw("sales_invoices/1#{id}/e_document_type") do |parsed_data, response|
+      self.class.get_raw("sales_invoices/#{id}/e_document_type") do |parsed_data, response|
         parsed_data[:data][:e_document_type]
       end
     end
