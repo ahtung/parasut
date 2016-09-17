2383851cd62488d3ad5ad7cac45de7bacbec3fd2
onur <onurkucukkece@gmail.com>
2016-09-17 18:01:08 +0300

    changed contacts spec

--- spec/parasut/contact_spec.rb
+++ spec/parasut/contact_spec.rb
@@ -7,37 +7,11 @@ RSpec.describe Parasut::Contact do
     let(:contact) { described_class.find(1) }
 
     it 'outstanding_payments' do
-      expect(contact.outstanding_payments)
-        .to eq(
-          [{
-            id: 1003,
-            date: '2014-06-25',
-            amount: '224.8',
-            notes: 'Is bitimi',
-            flow: 'in',
-            paid_on: nil,
-            payable_type: 'SalesInvoice',
-            payable_id: 502
-          }]
-        )
+      expect(contact.outstanding_payments).to be_instance_of(Parasut::Payment)
     end
 
     it 'past_transactions' do
-      expect(contact.past_transactions)
-        .to eq(
-          [{
-            account_id: nil,
-            account_name: nil,
-            amount: '200.0',
-            date: '2014-05-19',
-            debit_credit: nil,
-            description: 'On odeme',
-            issue_date: nil,
-            id: 2003,
-            item_id: 502,
-            transaction_type: 'collection'
-          }]
-        )
+      expect(contact.past_transactions).to be_instance_of(Parasut::Transaction)
     end
   end
 end
