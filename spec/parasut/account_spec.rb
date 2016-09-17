597e175c8f45e444bd87876f5609b1d8691abd6e
onur <onurkucukkece@gmail.com>
2016-09-17 18:01:01 +0300

    accounts specs

--- spec/parasut/account_spec.rb
+++ spec/parasut/account_spec.rb
@@ -2,4 +2,17 @@ require 'spec_helper'
 
 RSpec.describe Parasut::Account do
   it_behaves_like 'her model'
+
+  describe '#' do
+    let(:account) { described_class.find(1) }
+
+    it 'e_document_status' do
+      expect(account.transactions).to be_instance_of(Parasut::Transaction)
+    end
+
+    it 'e_document_type' do
+      expect(sales_invoice.e_document_type).to eq('e_invoice')
+    end
+  end
 end
+
